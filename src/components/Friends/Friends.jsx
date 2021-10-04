f2c292fe53ed8e65f3ad7c04c741b9b39f2eb080
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-10-04 14:27:17 +0300

    commit

--- src/components/Friends/Friends.jsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import friends from "../Friends/data/friends.json";
--- src/index.js
+++ src/index.js
@@ -1,40 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" },
-  { id: "id-4", name: "Redux in depth" },
-];
+const element = React.createElement("div", {
+  a: 1,
+  b: 2,
+  children: "hello world",
+});
 
-const BookList = ({ books }) => (
-  <ul>
-    {books.map((book) => (
-      <li>{book.name}</li>
-    ))}
-  </ul>
-);
+console.log(element);
 
-ReactDOM.render(
-  <BookList books={favouriteBooks} />,
-  document.getElementById("root")
-);
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+ReactDOM.render(element, document.querySelector("#root"));
